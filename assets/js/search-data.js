9126ea3c3bc7a4abd8247e3260df9255627da6ab
nicobers <115112345+nicobers@users.noreply.github.com>
2025-12-09 21:21:51 +0000

    Deploying to gh-pages from @ nicobers/nicobers.github.io@972bccf19c63ad9078d0e8a5fadbba1f764411bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,7 +52,18 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{
+    },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-honorable-mention-for-universities-space-research-association-distinguished-undergraduate-award",
+          title: 'Honorable mention for Universities Space Research Association Distinguished Undergraduate Award.',
+          description: "",
+          section: "News",},{id: "news-probing-the-peak-of-star-formation-with-the-stochastic-background-of-binary-black-hole-mergers-accepted-to-the-astrophysical-journal",
+          title: 'Probing the peak of star formation with the stochastic background of binary black...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2.html";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
